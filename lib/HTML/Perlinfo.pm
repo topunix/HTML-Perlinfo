abfca2c6d832f2d1c7abe81e5d1df418a08a62c8
Mike Accardo <perlhero@yahoo.com>
2015-06-06 17:58:05 -0400

    Update Perlinfo.pm

--- lib/HTML/Perlinfo.pm
+++ lib/HTML/Perlinfo.pm
@@ -12,7 +12,7 @@ use HTML::Perlinfo::Common;
 use base qw(Exporter HTML::Perlinfo::Base);
 our @EXPORT = qw(perlinfo);
 
-our $VERSION = '1.64';
+our $VERSION = '1.65';
 
 sub perlinfo {
   my ($opt) = @_;
